e0cb31a247d95f06009b418b8086cec873d40d70
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-07-01 13:29:18 +0800

    Update revenueSystem.js

--- scripts/revenueSystem.js
+++ scripts/revenueSystem.js
@@ -1,13 +1,1 @@
-function addRevenue() {
-  var revenueDisplay = document.getElementById('revenue');
 
-  var savedRevenueValue = localStorage.getItem('savedRevenueValue') || 0;
-
-  var currentRevenueValue = parseInt(savedRevenueValue);
-
-  var newRevenueValue = currentRevenueValue + 1;
-
-  revenueDisplay.innerHTML = newRevenueValue;
-
-  localStorage.setItem('savedRevenueValue', newRevenueValue);
-}
